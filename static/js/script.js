4ef385d42ec5afcde4b710d743432d327f97cdfe
EiriniChalatsiadou <>
2023-08-09 19:40:06 +0000

    add active menu

--- static/js/script.js
+++ static/js/script.js
@@ -3,6 +3,30 @@
 // });
 
 
+
+$(document).ready(
+    function () {
+        const dictionary = {
+            '/': 'homeMenu',
+            '/treatments': 'treatmentsMenu',
+            '/book-a-treatment': 'bookTreatmentMenu',
+            '/contact': 'contactMenu',
+            '/about-us': 'aboutUsMenu',
+            '/accounts/logout/': 'logoutMenu',
+            '/accounts/login/': 'loginMenu',
+        };
+        const currentPage = window.location.pathname;
+        const menuId = dictionary[currentPage];
+
+        $('#navbarNav li>a').removeClass('active');
+        $(`#${menuId}`).addClass('active');
+
+        console.log(currentPage, menuId, `#${menuId}`);
+    }
+
+
+);
+
 if (window.location.pathname == "/contact") {
     //initialise map
     let map;
