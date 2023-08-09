f787f31cca194b78f0621f7b613716285932567b
EiriniChalatsiadou <>
2023-08-09 22:42:09 +0000

    created db models and migrations

--- treatments/admin.py
+++ treatments/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import TreatmentType, Treatment
 
 # Register your models here.
+admin.site.register(TreatmentType)
+admin.site.register(Treatment)
--- treatments/models.py
+++ treatments/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class TreatmentType(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False)
+
+class Treatment(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False)
+    price = models.FloatField(null=False, blank=False, default=0.0)
+    treatmentType = models.ForeignKey(TreatmentType, on_delete=models.CASCADE)
+
+
+
