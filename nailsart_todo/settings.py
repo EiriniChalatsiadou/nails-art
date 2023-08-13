c775384c8b84f1e5146c29ebec27809e0584d407
EiriniChalatsiadou <>
2023-08-13 03:10:52 +0000

    test heroku

--- nailsart_todo/settings.py
+++ nailsart_todo/settings.py
@@ -163,12 +163,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_URL = '/static/media/'
+MEDIA_URL = '/media/'
+if not DEBUG:
+    MEDIA_URL = '/static/media/'
+
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 STATICFILES_DIRS = [
